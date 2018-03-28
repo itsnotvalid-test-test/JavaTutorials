4f4793631a5ba503f4443a6be53d600ec8d17095
itsnotvalid-test-test <37855023+itsnotvalid-test-test@users.noreply.github.com>
2018-03-28 07:39:01 +0530

    Create Collections.java

---
+++ Collections.java
@@ -0,0 +1,12 @@
+List<String> listStrings = new ArrayList<String>();
+listStrings.add("One");
+listStrings.add("Two");
+listStrings.add("Three");
+listStrings.add("Four");
+System.out.println(listStrings);
+
+
+List<Object> listAnything = new ArrayList<Object>();
+List<String> listWords = new ArrayList<String>();
+List<Integer> listNumbers = new ArrayList<Integer>();
+List<String> linkedWords = new LinkedList<String>();
